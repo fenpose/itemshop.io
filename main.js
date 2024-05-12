25cc89e51247a585cd79f4677e64d2132dcfa456
kurasak1 <94231150+kurasak1@users.noreply.github.com>
2024-05-12 13:19:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('#btn').addEventListener('click',() => {
+    document.querySelector('#price').innerHTML = "Цена 100 000р."
+})
+document.querySelector('#btn1').addEventListener('click',() => {
+    document.querySelector('#price').innerHTML = "Цена 80 000р."
+})
